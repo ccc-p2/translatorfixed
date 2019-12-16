33080064bde2eec54a65907dd72b6a12509fb04a
Herman Ramos <hermanramos@Hermans-MacBook.local>
2019-12-16 14:24:51 -0600

    added http client model

--- P2Translator.Client/Controllers/HomeController.cs
+++ P2Translator.Client/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -11,6 +13,7 @@ namespace P2Translator.Client.Controllers
 {
     public class HomeController : Controller
     {
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
