983d1a2da72f69b83a8a81681a0c4e5b3ca85252
Jharv94 <johnharvey94@hotmail.com>
2019-12-16 10:44:32 -0600

    Added Scaffold

---
+++ P2Translator.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace P2Translator.Client.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
