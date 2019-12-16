1f82b972e98c5901b7f238b8956f5c672ab6d293
Sergio De Anda <35745672+Sergio-De-Anda@users.noreply.github.com>
2019-12-16 17:44:19 -0600

    update translatorcontroller
    can now get messages and create them

--- P2Translator.WebApi/Controllers/TranslatorController.cs
+++ P2Translator.WebApi/Controllers/TranslatorController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using P2Translator.Data;
+using P2Translator.Data.Models;
+using P2Translator.WebApi.Models;
+using System;
 
 namespace P2Translator.WebApi.Controllers 
 {
@@ -35,6 +38,40 @@ namespace P2Translator.WebApi.Controllers
         Content = c.Content,
         MessageDateTime = c.MessageDateTime
       }).ToList();
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetMessages()
+    {
+      return await Task.FromResult(Ok(_db.Message.ToList()));
+    }
+
+    [HttpGet("{language}")]
+    public async Task<IActionResult> GetMessages(string language)
+    {
+      List<Message> messages = _db.Message.ToList();
+      List<Message> translatedMessageList = new List<Message>();
+      Translator tr = new Translator();
+      foreach(Message m in messages)
+      {
+        Message translatedMessage = new Message();
+        translatedMessage.Content = await tr.Translate(m.Content, language);
+        translatedMessage.MessageDateTime = m.MessageDateTime;
+        translatedMessage.MessageId = m.MessageId;
+        translatedMessageList.Add(translatedMessage);
+      }
+      return await Task.FromResult(Ok(translatedMessageList));
+    }
+    [HttpPost]
+    public async Task<IActionResult> Post(Message m)
+    {
+      if(ModelState.IsValid)
+      {
+        m.MessageDateTime = DateTime.Now;
+        _db.Message.Add(m);
+        await _db.SaveChangesAsync();
+        return await Task.FromResult(Ok(m));
+      }
+      return await Task.FromResult(NotFound(m));
     }
   }
 }
