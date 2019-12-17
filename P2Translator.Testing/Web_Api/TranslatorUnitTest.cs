3c5d1d13478904fe55cf3a8b39c848cd0f824c1a
Jharv94 <johnharvey94@hotmail.com>
2019-12-16 20:52:00 -0600

    added back unit tests

--- P2Translator.Testing/Web_Api/TranslatorUnitTest.cs
+++ P2Translator.Testing/Web_Api/TranslatorUnitTest.cs
@@ -1,64 +1,64 @@
-// using P2Translator.WebApi.Models;
-// using Xunit;
+using P2Translator.WebApi.Models;
+using Xunit;
 
-// namespace P2Translator.Testing.Web_Api
-// {
-//   public class TranslatorUnitTest
-//   {
-//     [Fact]
-//     public void Test_ValidMessageResponse()
-//     {
-//       // Arrange
-//       string messageRequest = "valid message";
-//       string messageResponse = "mensaje válido";
+namespace P2Translator.Testing.Web_Api
+{
+  public class TranslatorUnitTest
+  {
+    [Fact]
+    public void Test_ValidMessageResponse()
+    {
+      // Arrange
+      string messageRequest = "valid message";
+      string messageResponse = "mensaje válido";
 
-//       // Act out
-//       Translator actualResponse = new Translator();
+      // Act out
+      Translator actualResponse = new Translator();
       
-//       // Assert
-//       Assert.True(actualResponse.Translate(messageRequest, "spanish").Result.Equals(messageResponse));
-//     }
+      // Assert
+      Assert.True(actualResponse.Translate(messageRequest, "spanish").Result.Equals(messageResponse));
+    }
 
-//     [Fact]
-//     public void Test_EmptyMessage()
-//     {
-//       // Arrange
-//       string messageRequest = "";
+    [Fact]
+    public void Test_EmptyMessage()
+    {
+      // Arrange
+      string messageRequest = "";
 
-//       // Act out
-//       Translator actualResponse = new Translator();
+      // Act out
+      Translator actualResponse = new Translator();
 
-//       // Assert
-//       Assert.True(string.IsNullOrEmpty(actualResponse.Translate(messageRequest, "Spanish").Result));
-//     }
+      // Assert
+      Assert.True(string.IsNullOrEmpty(actualResponse.Translate(messageRequest, "Spanish").Result));
+    }
 
-//     [Fact]
-//     public void Test_NonexistingLanguage()
-//     {
-//       // Arrange
-//       string messageRequest = "valid message";
-//       string dummyLanguage = "dummy language";
+    [Fact]
+    public void Test_NonexistingLanguage()
+    {
+      // Arrange
+      string messageRequest = "valid message";
+      string dummyLanguage = "dummy language";
 
-//       // Act out
-//       Translator actualResponse = new Translator();
+      // Act out
+      Translator actualResponse = new Translator();
 
-//       // Assert
-//       Assert.True(string.IsNullOrEmpty(actualResponse.Translate(messageRequest, dummyLanguage).Result));
-//     }
+      // Assert
+      Assert.True(string.IsNullOrEmpty(actualResponse.Translate(messageRequest, dummyLanguage).Result));
+    }
 
-//     [Fact]
-//     public void Test_EmptyLanguage()
-//     {
-//       // Arrange
-//       string messageRequest = "valid message";
-//       string emptyLanguage = "";
+    [Fact]
+    public void Test_EmptyLanguage()
+    {
+      // Arrange
+      string messageRequest = "valid message";
+      string emptyLanguage = "";
 
-//       // Act out
-//       Translator actualResponse = new Translator();
+      // Act out
+      Translator actualResponse = new Translator();
 
-//       // Assert
-//       Assert.True(string.IsNullOrEmpty(actualResponse.Translate(messageRequest, emptyLanguage).Result));
-//     }
+      // Assert
+      Assert.True(string.IsNullOrEmpty(actualResponse.Translate(messageRequest, emptyLanguage).Result));
+    }
 
-//   }
-// }
+  }
+}
